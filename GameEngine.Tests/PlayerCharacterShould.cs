0824d335bef5275a2524e981f84cce310d9ff988
azurefaculty <azurefaculty@example.com>
2024-02-13 13:35:40 +0530

    v 3.0

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -17,5 +17,18 @@ namespace GameEngine.Tests
             sut.LastName = "2024";
             Assert.Equal("11feb 2024",sut.FullName);
         }
+
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(1, 99)]
+        [InlineData(50, 50)]
+        [InlineData(101, 1)]
+        public void TakeDamage(int damage, int expectedHealth)
+        {
+            var sut = new PlayerCharacter();
+            sut.TakeDamage(damage);
+
+            Assert.Equal(expectedHealth, sut.Health);
+        }
     }
 }
